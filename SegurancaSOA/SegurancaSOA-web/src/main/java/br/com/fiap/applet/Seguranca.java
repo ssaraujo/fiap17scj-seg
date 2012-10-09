f260de84015897818e36e5ad45c91d0be6a934dc
Silvio <Silvio@Silvio-PC>
2012-10-09 00:02:01 -0300

    applet

---
+++ SegurancaSOA/SegurancaSOA-web/src/main/java/br/com/fiap/applet/Seguranca.java
@@ -0,0 +1,126 @@
+package br.com.fiap.applet;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class Seguranca {
+
+	KeyPair keyPair = null;
+	String algorithm = "DSA";
+
+	public Seguranca() {
+		try {
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance(algorithm);
+			keyGen.initialize(1024);
+			keyPair = keyGen.genKeyPair();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getChavePublica() {
+		PublicKey publicKey = keyPair.getPublic();
+		return new String(DatatypeConverter.printBase64Binary(publicKey
+				.getEncoded()));
+	}
+
+	public String getChavePrivada() {
+		PrivateKey privateKey = keyPair.getPrivate();
+		System.out.println(privateKey.getEncoded());
+		return new String(DatatypeConverter.printBase64Binary(privateKey
+				.getEncoded()));
+	}
+
+	public String criptografar(String chave, String hash) {
+		KeyFactory keyFactory;
+		try {
+			keyFactory = KeyFactory.getInstance(algorithm);
+			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(
+					DatatypeConverter.parseBase64Binary(chave));
+			PrivateKey privateKey = keyFactory
+					.generatePrivate(pkcs8EncodedKeySpec);
+
+			byte[] ba = hash.getBytes("UTF8");
+			Signature sig = Signature.getInstance(algorithm);
+			sig.initSign(privateKey);
+			sig.update(ba);
+			byte[] signedData = sig.sign();
+			// return new String(signedData);
+			return DatatypeConverter.printBase64Binary(signedData);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SignatureException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	public boolean verificarAssinatura(String chave, String hash, String hashAssinado) {
+		try {
+			KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+			X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(
+					DatatypeConverter.parseBase64Binary(chave));
+			PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+			byte[] ba = hash.getBytes("UTF8");
+			Signature sig = Signature.getInstance(algorithm);
+			sig.initVerify(publicKey);
+			sig.update(ba);
+			return sig.verify(DatatypeConverter.parseBase64Binary(hashAssinado));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SignatureException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public String gerarMD5(String conteudo){		
+		try {
+			MessageDigest md;
+			md = MessageDigest.getInstance("MD5");
+			byte[] criptografada = md.digest(conteudo.getBytes());
+		    return new String(criptografada);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return null;
+	}
+
+}
