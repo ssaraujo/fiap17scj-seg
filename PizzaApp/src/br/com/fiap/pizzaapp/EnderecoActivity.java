f91cffffe882a717e417dc5b25ed438d4d19a28f
Silvio <Silvio@Silvio-PC>
2013-01-06 23:50:12 -0200

    silvio6

--- PizzaApp/src/br/com/fiap/pizzaapp/EnderecoActivity.java
+++ PizzaApp/src/br/com/fiap/pizzaapp/EnderecoActivity.java
@@ -30,7 +30,7 @@ import android.widget.TextView;
  *
  */
 public class EnderecoActivity extends Activity implements OnClickListener{
-	//teste silvio5
+	//teste silvio6
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
